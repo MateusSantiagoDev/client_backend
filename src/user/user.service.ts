f561aaf57212e9facf4c56cdd54b097c9ba6fd69
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-01-27 18:00:58 -0300

    chore: implemented service methods

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,61 @@
 import { Injectable } from "@nestjs/common";
+import { randomUUID } from "crypto";
+import { CreateUserDto } from "./dto/create-user-dto";
+import { UpdateUserDto } from "./dto/update-user-dto";
+import { UserEntity } from "./entities/entity-user";
 import { UserRepositoty } from "./user.repository";
 
 @Injectable()
 export class UserService {
     constructor(private readonly repository: UserRepositoty) {}
+
+    async create(dto: CreateUserDto): Promise<UserEntity> {
+      const newUser: UserEntity = { ...dto, id: randomUUID() }
+      if(!newUser) {
+        return
+      }
+      const result = await this.repository.create(newUser)
+      if(!result) {
+        return
+      }
+      return result;
+    }
+
+    async findAll(): Promise<UserEntity[]> {
+        const result = await this.repository.findAll()
+        if(!result) {
+            return
+        }
+        return result;
+    }
+
+    async findById(id: string): Promise<UserEntity> {
+        if(!id) {
+            return
+        }
+        const result = await this.repository.findById(id)
+        if(!result) {
+           return 
+        }
+        return result;
+    }
+
+    async update(id: string, dto: UpdateUserDto): Promise<UserEntity> {
+        await this.findById(id)
+        const data: Partial<UserEntity> = { ...dto };
+        const result = await this.repository.update(id, data)
+        if(!result) {
+            return
+        }
+        return result;
+    }
+
+    async delete(id: string): Promise<UserEntity> {
+        await this.findById(id)
+        const result = await this.repository.delete(id)
+        if(!result) {
+            return
+        }
+        return result;
+    }
 }
