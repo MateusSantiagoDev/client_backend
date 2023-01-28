feadb9045be68d6c8a6ab6d8fbd106ecab26be38
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-01-28 12:10:55 -0300

    chore: implemented 'financialModule'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FinancialModule } from './financial/Financial.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductModule } from './product/Product.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, ProductModule],
+  imports: [PrismaModule, UserModule, ProductModule, FinancialModule],
   controllers: [AppController],
   providers: [AppService],
 })
