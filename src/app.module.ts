4889a25a52bf0840840223a4f814b3e8de885bb5
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-01-29 22:13:39 -0300

    chore: created 'receiveModule' and imported 'prisma'

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,12 @@ import { FinancialModule } from './financial/Financial.module';
 import { PayModule } from './pay/Pay.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductModule } from './product/Product.module';
+import { ReceiveModule } from './receive/Receive.module';
 import { SangriaModule } from './sangria/sangria.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, ProductModule, FinancialModule, SangriaModule, PayModule],
+  imports: [PrismaModule, UserModule, ProductModule, FinancialModule, SangriaModule, PayModule, ReceiveModule],
   controllers: [AppController],
   providers: [AppService],
 })
