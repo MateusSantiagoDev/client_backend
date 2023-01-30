c3353a1bbacabc723fef18d8ed8e6e6cad3df3fd
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-01-30 10:31:14 -0300

    implemented error handling in the method of receive

--- src/receive/Receive.controller.ts
+++ src/receive/Receive.controller.ts
@@ -1,75 +1,84 @@
 import {
-    Controller,
-    Post,
-    Body,
-    Get,
-    Param,
-    Patch,
-    Delete,
-  } from '@nestjs/common';
-  import { ApiOperation, ApiTags } from '@nestjs/swagger';
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { HandleException } from 'src/exceptions/handleException';
 import { CreateReceiveDto } from './dto/create-receive-dto';
 import { UpdateReceiveDto } from './dto/update-receive-dto';
 import { ReceiveEntity } from './entities/ReceiveEntity';
 import { ReceiveService } from './Receive.service';
 
+@ApiTags('Receive')
+@Controller('receive')
+export class ReceiveController {
+  constructor(private readonly service: ReceiveService) {}
 
-  
-  @ApiTags('Receive')
-  @Controller('receive')
-  export class ReceiveController {
-    constructor(private readonly service: ReceiveService) {}
-  
-    @ApiOperation({
-      summary: 'Adicionar um novo recebimento',
-    })
-    @Post()
-    async create(@Body() dto: CreateReceiveDto): Promise<ReceiveEntity> {
-      try {
-        return await this.service.create(dto);
-      } catch (err) {}
-    }
-  
-    @ApiOperation({
-      summary: 'Visualizar todos os recebimentos',
-    })
-    @Get()
-    async findAll(): Promise<ReceiveEntity[]> {
-      try {
-        return await this.service.findAll();
-      } catch (err) {}
-    }
-  
-    @ApiOperation({
-      summary: 'Visualizar um recebimento pelo ID',
-    })
-    @Get(':id')
-    async findById(@Param('id') id: string): Promise<ReceiveEntity> {
-      try {
-        return await this.service.findById(id);
-      } catch (err) {}
-    }
-  
-    @ApiOperation({
-      summary: 'Atualizar um recebimento pelo ID',
-    })
-    @Patch(':id')
-    async update(
-      @Param('id') id: string,
-      @Body() dto: UpdateReceiveDto,
-    ): Promise<ReceiveEntity> {
-      try {
-        return await this.service.update(id, dto);
-      } catch (err) {}
-    }
-  
-    @ApiOperation({
-      summary: 'Excluir um recebimento pelo ID',
-    })
-    @Delete(':id')
-    async delete(@Param('id') id: string): Promise<ReceiveEntity> {
-      try {
-        return await this.service.delete(id);
-      } catch (err) {}
+  @ApiOperation({
+    summary: 'Adicionar um novo recebimento',
+  })
+  @Post()
+  async create(@Body() dto: CreateReceiveDto): Promise<ReceiveEntity> {
+    try {
+      return await this.service.create(dto);
+    } catch (err) {
+      HandleException(err);
     }
   }
+
+  @ApiOperation({
+    summary: 'Visualizar todos os recebimentos',
+  })
+  @Get()
+  async findAll(): Promise<ReceiveEntity[]> {
+    try {
+      return await this.service.findAll();
+    } catch (err) {
+      HandleException(err);
+    }
+  }
+
+  @ApiOperation({
+    summary: 'Visualizar um recebimento pelo ID',
+  })
+  @Get(':id')
+  async findById(@Param('id') id: string): Promise<ReceiveEntity> {
+    try {
+      return await this.service.findById(id);
+    } catch (err) {
+      HandleException(err);
+    }
+  }
+
+  @ApiOperation({
+    summary: 'Atualizar um recebimento pelo ID',
+  })
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() dto: UpdateReceiveDto,
+  ): Promise<ReceiveEntity> {
+    try {
+      return await this.service.update(id, dto);
+    } catch (err) {
+      HandleException(err);
+    }
+  }
+
+  @ApiOperation({
+    summary: 'Excluir um recebimento pelo ID',
+  })
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<ReceiveEntity> {
+    try {
+      return await this.service.delete(id);
+    } catch (err) {
+      HandleException(err);
+    }
+  }
+}
--- src/receive/Receive.service.ts
+++ src/receive/Receive.service.ts
@@ -1,62 +1,57 @@
-import { Injectable } from "@nestjs/common";
-import { randomUUID } from "crypto";
-import { CreateReceiveDto } from "./dto/create-receive-dto";
-import { UpdateReceiveDto } from "./dto/update-receive-dto";
-import { ReceiveEntity } from "./entities/ReceiveEntity";
-import { ReceiveRepository } from "./Receive.repository";
-
+import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { Exceptions } from 'src/exceptions/exception';
+import { Exception } from 'src/exceptions/exception.type';
+import { CreateReceiveDto } from './dto/create-receive-dto';
+import { UpdateReceiveDto } from './dto/update-receive-dto';
+import { ReceiveEntity } from './entities/ReceiveEntity';
+import { ReceiveRepository } from './Receive.repository';
 
 @Injectable()
 export class ReceiveService {
-    constructor(private readonly repository: ReceiveRepository) {}
+  constructor(private readonly repository: ReceiveRepository) {}
 
-    async create(dto: CreateReceiveDto): Promise<ReceiveEntity> {
-      const data: ReceiveEntity = { ...dto, id: randomUUID() }
-      if(!data) {
-        return
-      }
-      const result = await this.repository.create(data)
-      if(!result) {
-        return
-      }
-      return result;
+  async create(dto: CreateReceiveDto): Promise<ReceiveEntity> {
+    try {
+      const data: ReceiveEntity = { ...dto, id: randomUUID() };
+      return await this.repository.create(data);
+    } catch (err) {
+      throw new Exceptions(Exception.UnprocessableEntityException);
     }
+  }
 
-    async findAll(): Promise<ReceiveEntity[]> {
-        const result = await this.repository.findAll()
-        if(!result) {
-            return
-        }
-        return result;
+  async findAll(): Promise<ReceiveEntity[]> {
+    try {
+      return await this.repository.findAll();
+    } catch (err) {
+      throw new Exceptions(Exception.DatabaseException);
     }
+  }
 
-    async findById(id: string): Promise<ReceiveEntity> {
-        if(!id) {
-            return
-        }
-        const result = await this.repository.findById(id)
-        if(!result) {
-           return 
-        }
-        return result;
+  async findById(id: string): Promise<ReceiveEntity> {
+    try {
+      return await this.repository.findById(id);
+    } catch (err) {
+      throw new Exceptions(Exception.NotFoundException);
     }
+  }
 
-    async update(id: string, dto: UpdateReceiveDto): Promise<ReceiveEntity> {
-        await this.findById(id)        
-        const data: Partial<ReceiveEntity> = { ...dto };
-        const result = await this.repository.update(id, data)
-        if(!result) {
-            return
-        }
-        return result;
+  async update(id: string, dto: UpdateReceiveDto): Promise<ReceiveEntity> {
+    await this.findById(id);
+    try {
+      const data: Partial<ReceiveEntity> = { ...dto };
+      return await this.repository.update(id, data);
+    } catch (err) {
+      throw new Exceptions(Exception.UnprocessableEntityException);
     }
+  }
 
-    async delete(id: string): Promise<ReceiveEntity> {
-        await this.findById(id)
-        const result = await this.repository.delete(id)
-        if(!result) {
-            return
-        }
-        return result;
+  async delete(id: string): Promise<ReceiveEntity> {
+    await this.findById(id);
+    try {
+      return await this.repository.delete(id);
+    } catch (err) {
+      throw new Exceptions(Exception.NotFoundException);
     }
+  }
 }
