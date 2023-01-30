665d5df970e8fdba72ce6f2b30b2f4b91f6b477d
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-01-30 14:41:45 -0300

    feat: remove password

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -4,25 +4,34 @@ import { UserEntity } from './entities/entity-user';
 
 @Injectable()
 export class UserRepositoty {
+  private Select = {
+    id: true,
+    name: true,
+    cpf: false,
+    rg: false,
+    contact: true,
+    role: false,
+    password: false,
+  }
   constructor(private readonly prisma: PrismaService) {}
 
   async create(data: UserEntity): Promise<UserEntity> {
-    return await this.prisma.user.create({ data });
+    return await this.prisma.user.create({ data, select: this.Select });
   }
 
   async findAll(): Promise<UserEntity[]> {
-    return await this.prisma.user.findMany();
+    return await this.prisma.user.findMany({ select: this.Select });
   }
 
   async findById(id: string): Promise<UserEntity> {
-    return await this.prisma.user.findFirstOrThrow({ where: { id } });
+    return await this.prisma.user.findFirstOrThrow({ where: { id }, select: this.Select });
   }
 
   async update(id: string, data: Partial<UserEntity>): Promise<UserEntity> {
-    return await this.prisma.user.update({ where: { id }, data });
+    return await this.prisma.user.update({ where: { id }, data, select: this.Select });
   }
 
   async delete(id: string): Promise<UserEntity> {
-    return await this.prisma.user.delete({ where: { id } });
+    return await this.prisma.user.delete({ where: { id }, select: this.Select });
   }
 }
