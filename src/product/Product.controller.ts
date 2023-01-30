ddd03f850923bc98c8349b854212dca101c9dbad
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-01-30 10:44:38 -0300

    implemented error handling in the method of product

--- src/product/Product.controller.ts
+++ src/product/Product.controller.ts
@@ -1,74 +1,84 @@
 import {
-    Controller,
-    Post,
-    Body,
-    Get,
-    Param,
-    Patch,
-    Delete,
-  } from '@nestjs/common';
-  import { ApiOperation, ApiTags } from '@nestjs/swagger';
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { HandleException } from 'src/exceptions/handleException';
 import { CreateProductDto } from './dto/create-product-dto';
 import { UpdateProductDto } from './dto/update-product-dto';
 import { ProductEntity } from './entities/ProductEntity';
 import { ProductService } from './Product.service';
 
-  
-  @ApiTags('Product')
-  @Controller('product')
-  export class ProductController {
-    constructor(private readonly service: ProductService) {}
-  
-    @ApiOperation({
-      summary: 'Adicionar um novo Produto',
-    })
-    @Post()
-    async create(@Body() dto: CreateProductDto): Promise<ProductEntity> {
-      try {
-        return await this.service.create(dto);
-      } catch (err) {}
-    }
-  
-    @ApiOperation({
-      summary: 'Visualizar todos os Produtos',
-    })
-    @Get()
-    async findAll(): Promise<ProductEntity[]> {
-      try {
-        return await this.service.findAll();
-      } catch (err) {}
-    }
-  
-    @ApiOperation({
-      summary: 'Visualizar um Produto pelo ID',
-    })
-    @Get(':id')
-    async findById(@Param('id') id: string): Promise<ProductEntity> {
-      try {
-        return await this.service.findById(id);
-      } catch (err) {}
-    }
-  
-    @ApiOperation({
-      summary: 'Atualizar um Produto pelo ID',
-    })
-    @Patch(':id')
-    async update(
-      @Param('id') id: string,
-      @Body() dto: UpdateProductDto,
-    ): Promise<ProductEntity> {
-      try {
-        return await this.service.update(id, dto);
-      } catch (err) {}
-    }
-  
-    @ApiOperation({
-      summary: 'Excluir um Produto pelo ID',
-    })
-    @Delete(':id')
-    async delete(@Param('id') id: string): Promise<ProductEntity> {
-      try {
-        return await this.service.delete(id);
-      } catch (err) {}
+@ApiTags('Product')
+@Controller('product')
+export class ProductController {
+  constructor(private readonly service: ProductService) {}
+
+  @ApiOperation({
+    summary: 'Adicionar um novo Produto',
+  })
+  @Post()
+  async create(@Body() dto: CreateProductDto): Promise<ProductEntity> {
+    try {
+      return await this.service.create(dto);
+    } catch (err) {
+      HandleException(err);
     }
   }
+
+  @ApiOperation({
+    summary: 'Visualizar todos os Produtos',
+  })
+  @Get()
+  async findAll(): Promise<ProductEntity[]> {
+    try {
+      return await this.service.findAll();
+    } catch (err) {
+      HandleException(err);
+    }
+  }
+
+  @ApiOperation({
+    summary: 'Visualizar um Produto pelo ID',
+  })
+  @Get(':id')
+  async findById(@Param('id') id: string): Promise<ProductEntity> {
+    try {
+      return await this.service.findById(id);
+    } catch (err) {
+      HandleException(err);
+    }
+  }
+
+  @ApiOperation({
+    summary: 'Atualizar um Produto pelo ID',
+  })
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() dto: UpdateProductDto,
+  ): Promise<ProductEntity> {
+    try {
+      return await this.service.update(id, dto);
+    } catch (err) {
+      HandleException(err);
+    }
+  }
+
+  @ApiOperation({
+    summary: 'Excluir um Produto pelo ID',
+  })
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<ProductEntity> {
+    try {
+      return await this.service.delete(id);
+    } catch (err) {
+      HandleException(err);
+    }
+  }
+}
--- src/product/Product.service.ts
+++ src/product/Product.service.ts
@@ -1,62 +1,57 @@
-import { Injectable } from "@nestjs/common";
-import { randomUUID } from "crypto";
-import { CreateProductDto } from "./dto/create-product-dto";
-import { UpdateProductDto } from "./dto/update-product-dto";
-import { ProductEntity } from "./entities/ProductEntity";
-import { ProductRepository } from "./Product.repository";
-
+import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { Exceptions } from 'src/exceptions/exception';
+import { Exception } from 'src/exceptions/exception.type';
+import { CreateProductDto } from './dto/create-product-dto';
+import { UpdateProductDto } from './dto/update-product-dto';
+import { ProductEntity } from './entities/ProductEntity';
+import { ProductRepository } from './Product.repository';
 
 @Injectable()
 export class ProductService {
-    constructor(private readonly repository: ProductRepository) {}
+  constructor(private readonly repository: ProductRepository) {}
 
-    async create(dto: CreateProductDto): Promise<ProductEntity> {
-      const newUser: ProductEntity = { ...dto, id: randomUUID() }
-      if(!newUser) {
-        return
-      }
-      const result = await this.repository.create(newUser)
-      if(!result) {
-        return
-      }
-      return result;
+  async create(dto: CreateProductDto): Promise<ProductEntity> {
+    try {
+      const data: ProductEntity = { ...dto, id: randomUUID() };
+      return await this.repository.create(data);
+    } catch (err) {
+      throw new Exceptions(Exception.UnprocessableEntityException);
     }
+  }
 
-    async findAll(): Promise<ProductEntity[]> {
-        const result = await this.repository.findAll()
-        if(!result) {
-            return
-        }
-        return result;
+  async findAll(): Promise<ProductEntity[]> {
+    try {
+      return await this.repository.findAll();
+    } catch (err) {
+      throw new Exceptions(Exception.DatabaseException);
     }
+  }
 
-    async findById(id: string): Promise<ProductEntity> {
-        if(!id) {
-            return
-        }
-        const result = await this.repository.findById(id)
-        if(!result) {
-           return 
-        }
-        return result;
+  async findById(id: string): Promise<ProductEntity> {
+    try {
+      return await this.repository.findById(id);
+    } catch (err) {
+      throw new Exceptions(Exception.NotFoundException);
     }
+  }
 
-    async update(id: string, dto: UpdateProductDto): Promise<ProductEntity> {
-        await this.findById(id)        
-        const data: Partial<ProductEntity> = { ...dto };
-        const result = await this.repository.update(id, data)
-        if(!result) {
-            return
-        }
-        return result;
+  async update(id: string, dto: UpdateProductDto): Promise<ProductEntity> {
+    await this.findById(id);
+    try {
+      const data: Partial<ProductEntity> = { ...dto };
+      return await this.repository.update(id, data);
+    } catch (err) {
+      throw new Exceptions(Exception.UnprocessableEntityException);
     }
+  }
 
-    async delete(id: string): Promise<ProductEntity> {
-        await this.findById(id)
-        const result = await this.repository.delete(id)
-        if(!result) {
-            return
-        }
-        return result;
+  async delete(id: string): Promise<ProductEntity> {
+    await this.findById(id);
+    try {
+      return await this.repository.delete(id);
+    } catch (err) {
+      throw new Exceptions(Exception.NotFoundException);
     }
+  }
 }
