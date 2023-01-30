fa6b446b40131b42e751260ddaa962e382763772
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-01-30 10:52:31 -0300

    implemented error handling in the method of pay

--- src/pay/pay.controller.ts
+++ src/pay/pay.controller.ts
@@ -1,74 +1,84 @@
 import {
-    Controller,
-    Post,
-    Body,
-    Get,
-    Param,
-    Patch,
-    Delete,
-  } from '@nestjs/common';
-  import { ApiOperation, ApiTags } from '@nestjs/swagger';
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { HandleException } from 'src/exceptions/handleException';
 import { CreatePayDto } from './dto/create-pay-dto';
 import { UpdatePayDto } from './dto/update-pay-dto';
 import { PayEntity } from './entities/PayEntity';
 import { PayService } from './Pay.service';
 
-  
-  @ApiTags('Pay')
-  @Controller('pay')
-  export class PayController {
-    constructor(private readonly service: PayService) {}
-  
-    @ApiOperation({
-      summary: 'Adicionar um novo Pagamento',
-    })
-    @Post()
-    async create(@Body() dto: CreatePayDto): Promise<PayEntity> {
-      try {
-        return await this.service.create(dto);
-      } catch (err) {}
-    }
-  
-    @ApiOperation({
-      summary: 'Visualizar todos os Pagamentos',
-    })
-    @Get()
-    async findAll(): Promise<PayEntity[]> {
-      try {
-        return await this.service.findAll();
-      } catch (err) {}
-    }
-  
-    @ApiOperation({
-      summary: 'Visualizar um Pagamento pelo ID',
-    })
-    @Get(':id')
-    async findById(@Param('id') id: string): Promise<PayEntity> {
-      try {
-        return await this.service.findById(id);
-      } catch (err) {}
-    }
-  
-    @ApiOperation({
-      summary: 'Atualizar um Pagamento pelo ID',
-    })
-    @Patch(':id')
-    async update(
-      @Param('id') id: string,
-      @Body() dto: UpdatePayDto,
-    ): Promise<PayEntity> {
-      try {
-        return await this.service.update(id, dto);
-      } catch (err) {}
-    }
-  
-    @ApiOperation({
-      summary: 'Excluir um Pagamento pelo ID',
-    })
-    @Delete(':id')
-    async delete(@Param('id') id: string): Promise<PayEntity> {
-      try {
-        return await this.service.delete(id);
-      } catch (err) {}
+@ApiTags('Pay')
+@Controller('pay')
+export class PayController {
+  constructor(private readonly service: PayService) {}
+
+  @ApiOperation({
+    summary: 'Adicionar um novo Pagamento',
+  })
+  @Post()
+  async create(@Body() dto: CreatePayDto): Promise<PayEntity> {
+    try {
+      return await this.service.create(dto);
+    } catch (err) {
+      HandleException(err);
     }
   }
+
+  @ApiOperation({
+    summary: 'Visualizar todos os Pagamentos',
+  })
+  @Get()
+  async findAll(): Promise<PayEntity[]> {
+    try {
+      return await this.service.findAll();
+    } catch (err) {
+      HandleException(err);
+    }
+  }
+
+  @ApiOperation({
+    summary: 'Visualizar um Pagamento pelo ID',
+  })
+  @Get(':id')
+  async findById(@Param('id') id: string): Promise<PayEntity> {
+    try {
+      return await this.service.findById(id);
+    } catch (err) {
+      HandleException(err);
+    }
+  }
+
+  @ApiOperation({
+    summary: 'Atualizar um Pagamento pelo ID',
+  })
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() dto: UpdatePayDto,
+  ): Promise<PayEntity> {
+    try {
+      return await this.service.update(id, dto);
+    } catch (err) {
+      HandleException(err);
+    }
+  }
+
+  @ApiOperation({
+    summary: 'Excluir um Pagamento pelo ID',
+  })
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<PayEntity> {
+    try {
+      return await this.service.delete(id);
+    } catch (err) {
+      HandleException(err);
+    }
+  }
+}
--- src/pay/pay.service.ts
+++ src/pay/pay.service.ts
@@ -1,63 +1,57 @@
-import { Injectable } from "@nestjs/common";
-import { randomUUID } from "crypto";
-import { CreatePayDto } from "./dto/create-pay-dto";
-import { UpdatePayDto } from "./dto/update-pay-dto";
-import { PayEntity } from "./entities/PayEntity";
-import { PayRepository } from "./Pay.repository";
-
-
+import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { Exceptions } from 'src/exceptions/exception';
+import { Exception } from 'src/exceptions/exception.type';
+import { CreatePayDto } from './dto/create-pay-dto';
+import { UpdatePayDto } from './dto/update-pay-dto';
+import { PayEntity } from './entities/PayEntity';
+import { PayRepository } from './Pay.repository';
 
 @Injectable()
 export class PayService {
-    constructor(private readonly repository: PayRepository) {}
+  constructor(private readonly repository: PayRepository) {}
 
-    async create(dto: CreatePayDto): Promise<PayEntity> {
-      const data: PayEntity = { ...dto, id: randomUUID() }
-      if(!data) {
-        return
-      }
-      const result = await this.repository.create(data)
-      if(!result) {
-        return
-      }
-      return result;
+  async create(dto: CreatePayDto): Promise<PayEntity> {
+    try {
+      const data: PayEntity = { ...dto, id: randomUUID() };
+      return await this.repository.create(data);
+    } catch (err) {
+      throw new Exceptions(Exception.UnprocessableEntityException);
     }
+  }
 
-    async findAll(): Promise<PayEntity[]> {
-        const result = await this.repository.findAll()
-        if(!result) {
-            return
-        }
-        return result;
+  async findAll(): Promise<PayEntity[]> {
+    try {
+      return await this.repository.findAll();
+    } catch (err) {
+      throw new Exceptions(Exception.DatabaseException);
     }
+  }
 
-    async findById(id: string): Promise<PayEntity> {
-        if(!id) {
-            return
-        }
-        const result = await this.repository.findById(id)
-        if(!result) {
-           return 
-        }
-        return result;
+  async findById(id: string): Promise<PayEntity> {
+    try {
+      return await this.repository.findById(id);
+    } catch (err) {
+      throw new Exceptions(Exception.NotFoundException);
     }
+  }
 
-    async update(id: string, dto: UpdatePayDto): Promise<PayEntity> {
-        await this.findById(id)        
-        const data: Partial<PayEntity> = { ...dto };
-        const result = await this.repository.update(id, data)
-        if(!result) {
-            return
-        }
-        return result;
+  async update(id: string, dto: UpdatePayDto): Promise<PayEntity> {
+    await this.findById(id);
+    try {
+      const data: Partial<PayEntity> = { ...dto };
+      return await this.repository.update(id, data);
+    } catch (err) {
+      throw new Exceptions(Exception.UnprocessableEntityException);
     }
+  }
 
-    async delete(id: string): Promise<PayEntity> {
-        await this.findById(id)
-        const result = await this.repository.delete(id)
-        if(!result) {
-            return
-        }
-        return result;
+  async delete(id: string): Promise<PayEntity> {
+    await this.findById(id);
+    try {
+      return await this.repository.delete(id);
+    } catch (err) {
+      throw new Exceptions(Exception.NotFoundException);
     }
+  }
 }
