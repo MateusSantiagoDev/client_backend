85f14c9cedd83f98b4c942b5f125f0d011bdc9a0
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-01-29 21:31:58 -0300

    chore: implemented the payment method module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FinancialModule } from './financial/Financial.module';
+import { PayModule } from './pay/pay.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductModule } from './product/Product.module';
 import { SangriaModule } from './sangria/sangria.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, ProductModule, FinancialModule, SangriaModule],
+  imports: [PrismaModule, UserModule, ProductModule, FinancialModule, SangriaModule, PayModule],
   controllers: [AppController],
   providers: [AppService],
 })
